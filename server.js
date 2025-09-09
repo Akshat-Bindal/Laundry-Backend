ce6c272a74ef9b0a95e59e5809041c3ec4609a52
Akshat-Bindal <biakshat31@gmail.com>
2025-09-09 10:48:42 +0530

    cors install

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 import dotenv from 'dotenv';
 dotenv.config(); 
 import express from 'express';
-
+import cors from "cors"
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import serviceRoutes from './routes/serviceRoutes.js';
 import userRoutes from "./routes/userRoutes.js";
-import cartRoutes from "./routes/cartRoutes.js"
 
 connectDB();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://washingtons.in/" 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true
+}));
 
 const app = express();
 app.use(express.json());
@@ -21,7 +37,6 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/services', serviceRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/cart", cartRoutes);
 
 
 const PORT = process.env.PORT || 5000;
